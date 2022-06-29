3d36d282a50e57f6563462848b184d22cc2fc1a3
Kelly Li <kelly.li.keyu@gmail.com>
2022-06-29 13:23:12 -0600

    giftidea react

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ from flask import Flask, render_template, request, flash, session, redirect, jso
 from model import User, Gift, Hobby, UserHobby, Question, Answer, Liked
 from model import connect_to_db, db
 import crud
+import os
 from jinja2 import StrictUndefined
+import requests
 
 app = Flask(__name__)
 app.secret_key = "dev"
@@ -17,22 +19,22 @@ def homepage():
 
     return render_template("homepage.html")
 
-@app.route("/search")
-def search():
-    """View search page."""
+# @app.route("/search")
+# def search():
+#     """View search page."""
 
-    return render_template("search.html")
+#     return render_template("search.html")
 
-@app.route("/result", methods=['POST'])
-def search_result():
-    """View search result."""
-    gender = request.form.get('gender')
-    age = request.form.get('age')
-    price = int(request.form.get('price'))
-    hobby_ids = request.form.getlist('hobby')
-    gifts = crud.get_gifts(gender, age, price, hobby_ids)
+# @app.route("/result", methods=['POST'])
+# def search_result():
+#     """View search result."""
+#     gender = request.form.get('gender')
+#     age = request.form.get('age')
+#     price = int(request.form.get('price'))
+#     hobby_ids = request.form.getlist('hobby')
+#     gifts = crud.get_gifts(gender, age, price, hobby_ids)
 
-    return render_template("result.html", gifts=gifts, age=age, gender=gender,price=price)
+#     return render_template("result.html", gifts=gifts, age=age, gender=gender,price=price)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -101,100 +103,105 @@ def user_profile():
     return render_template("profile.html", email=email, username=username, gender=gender, 
                                             age=age, hobbies=hobbies,questions=questions)
 
-@app.route("/ask")
-def ask_page():
-    """View Ask page."""
+# @app.route("/ask")
+# def ask_page():
+#     """View Ask page."""
 
-    return render_template("ask.html")
+#     return render_template("ask.html")
 
-@app.route("/answers")
-def answer_page():
-    """View answer page."""
-    questions = crud.get_all_questions()
-    return render_template("questions-and-answers.html",questions=questions)
+# @app.route("/answers")
+# def answer_page():
+#     """View answer page."""
+#     questions = crud.get_all_questions()
+#     return render_template("questions-and-answers.html",questions=questions)
 
-@app.route("/post-question", methods=['POST'])
-def question_info():
-    """Create a new question."""
+# @app.route("/post-question", methods=['POST'])
+# def question_info():
+#     """Create a new question."""
 
-    logged_in_email = session.get("user_email")
-    gender = request.form.get('gender')
-    age = request.form.get('age')
-    price = int(request.form.get('price'))
-    hobby_name = request.form.get('hobby')
+#     logged_in_email = session.get("user_email")
+#     gender = request.form.get('gender')
+#     age = request.form.get('age')
+#     price = int(request.form.get('price'))
+#     hobby_name = request.form.get('hobby')
 
-    if logged_in_email is None:
-        flash("You must log in to ask a question.")
-        return redirect("/")
-    elif not gender or not age or not price or not hobby_name:
-        flash("Please select your requirements.")
-        return redirect("/")
-    else:
-        user = crud.get_user_by_email(logged_in_email)
-        question_type = True
-        new_question = crud.create_question(user,gender,age,price,hobby_name,question_type)
-        db.session.add(new_question)
-        db.session.commit()
+#     if logged_in_email is None:
+#         flash("You must log in to ask a question.")
+#         return redirect("/")
+#     elif not gender or not age or not price or not hobby_name:
+#         flash("Please select your requirements.")
+#         return redirect("/")
+#     else:
+#         user = crud.get_user_by_email(logged_in_email)
+#         question_type = True
+#         new_question = crud.create_question(user,gender,age,price,hobby_name,question_type)
+#         db.session.add(new_question)
+#         db.session.commit()
 
-        flash(f"You post a question!")
+#         flash(f"You post a question!")
 
-        questions = crud.get_all_questions()
+#         questions = crud.get_all_questions()
     
-        return render_template("questions-and-answers.html",questions=questions)
+#         return render_template("questions-and-answers.html",questions=questions)
 
 
-@app.route("/answer/<question_id>", methods=['POST'])
-def answer(question_id):
-    """Create a new answer."""
+# @app.route("/answer/<question_id>", methods=['POST'])
+# def answer(question_id):
+#     """Create a new answer."""
 
-    logged_in_email = session.get("user_email")
-    gift_name = request.form.get('gift_name')
+#     logged_in_email = session.get("user_email")
+#     gift_name = request.form.get('gift_name')
     
-    if logged_in_email is None:
-        flash("You must log in to answer a question.")
-        return redirect("/")
-    elif not gift_name:
-        flash("Please give your answer.")
-        return redirect("/")
-    else:
-        user = crud.get_user_by_email(logged_in_email)
-        new_answer = crud.create_answer(user,gift_name,question_id)
+#     if logged_in_email is None:
+#         flash("You must log in to answer a question.")
+#         return redirect("/")
+#     elif not gift_name:
+#         flash("Please give your answer.")
+#         return redirect("/")
+#     else:
+#         user = crud.get_user_by_email(logged_in_email)
+#         new_answer = crud.create_answer(user,gift_name,question_id)
         
-        db.session.add(new_answer)
-        db.session.commit()
+#         db.session.add(new_answer)
+#         db.session.commit()
 
-        flash(f"You post an answer!")
+#         flash(f"You post an answer!")
 
-        return render_template("your_answer.html",answer=new_answer)
+#         return render_template("your_answer.html",answer=new_answer)
 
-@app.route("/like/<answer_id>", methods=['POST'])
-def likes(answer_id):
-    """Create a new like."""
+# @app.route("/like/<answer_id>", methods=['POST'])
+# def likes(answer_id):
+#     """Create a new like."""
 
-    logged_in_email = session.get("user_email")
-    user_like = request.form.get("user_like")
+#     logged_in_email = session.get("user_email")
+#     user_like = request.form.get("user_like")
 
-    if logged_in_email is None:
-        flash("You must log in to like an answer.")
-        return redirect("/")
-    elif not user_like:
-        flash("Please like an answer.")
-        return redirect("/")
-    else:
-        user = crud.get_user_by_email(logged_in_email)
-        new_like = crud.create_like(user,answer_id)
+#     if logged_in_email is None:
+#         flash("You must log in to like an answer.")
+#         return redirect("/")
+#     elif not user_like:
+#         flash("Please like an answer.")
+#         return redirect("/")
+#     else:
+#         user = crud.get_user_by_email(logged_in_email)
+#         new_like = crud.create_like(user,answer_id)
         
-        db.session.add(new_like)
-        db.session.commit()
+#         db.session.add(new_like)
+#         db.session.commit()
 
-        flash(f"You liked an answer!")
+#         flash(f"You liked an answer!")
 
-        return render_template("your_like.html",like=new_like)
+#         return render_template("your_like.html",like=new_like)
 
 @app.route("/react")
 def react():
 
     return render_template("questions.html")
+
+@app.route("/search-react")
+def search_react():
+
+    return render_template("search_results.html")
 
 @app.route("/questions", methods=['POST'])
 def add_new_question():
@@ -268,6 +275,53 @@ def add_new_like():
 
         flash(f"You liked an answer!")
         return jsonify({"success": True, "likeAdded": new_like.answer_id})
+
+API_KEY = os.environ['AMAZON_KEY']
+URL = "https://amazon-products1.p.rapidapi.com/search"
+
+HEADERS = {
+	"X-RapidAPI-Key": "984f9bfe66msh74d4b30163a4b64p1e5502jsnbe8887aadb9b",
+	"X-RapidAPI-Host": "amazon-products1.p.rapidapi.com"
+}
+@app.route("/search", methods=['POST'])
+def search():
+    # logged_in_email = session.get("user_email")
+    gender = request.get_json().get('gender')
+    age = request.get_json().get('age')
+    price = int(request.get_json().get('price'))
+    hobby_name = request.get_json().get('hobby')
+
+    querystring = {"country":"US","query":f"gift+{gender}+{age}+year+old+{hobby_name}"}
+
+    response = requests.request("GET", URL, headers=HEADERS, params=querystring)
+    results = response.text.results
+    filter_results = []
+
+    for result in results:
+        if result.prices.current_price <= price:
+            filter_results.append({
+                "title": result.title,
+                "image" : result.image,
+                "full_link" : result.full_link,
+                "price" : result.prices.current_price,
+                "reviews" : result.reviews
+            })
+
+    return jsonify({"success": True, "searchResults": filter_results})
+    # if logged_in_email is None:
+    #     flash("You must log in to ask a question.")
+    #     return jsonify({"success": False, "message":"You must log in to ask a question."})
+    # elif not gender or not age or not price or not hobby_name:
+    #     flash("Please select your requirements.")
+    #     return jsonify({"success": False, "message":"Please select your requirements."})
+    
+        # user = crud.get_user_by_email(logged_in_email)
+        
+        
+        
+        
+
+   
 
 if __name__ == "__main__":
     connect_to_db(app)
---
+++ static/js/searchResult.jsx
@@ -0,0 +1,158 @@
+"use strict";
+
+function Search(props) {
+  const [criteria, setCriteria] = React.useState({
+    gender: "female",
+    age: "0-2",
+    price: "10",
+    hobby: "art",
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setCriteria((prevCriteria) => ({
+      ...prevCriteria,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("/search", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(criteria),
+    })
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        const searchResults = jsonResponse.searchResults;
+        props.addResults(searchResults);
+      });
+  };
+  return (
+    <React.Fragment>
+      <h2>Search a gift</h2>
+      <div className="search-criteria">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Choose a gender:
+            <select
+              value={criteria.gender}
+              name="gender"
+              onChange={handleChange}
+            >
+              <option value="female" name="gender" id="gender-female">
+                Female
+              </option>
+              <option value="male" name="gender" id="gender-male">
+                Male
+              </option>
+              <option value="either gender" name="gender" id="gender-either">
+                Either gender
+              </option>
+              <option value="couple" name="gender" id="gender-couple">
+                Couple
+              </option>
+            </select>
+          </label>
+          <label>
+            Choose an age:
+            <select value={criteria.age} name="age" onChange={handleChange}>
+              <option value="0-2" name="age" id="age-1">
+                0-2
+              </option>
+              <option value="2-6" name="age" id="age-2">
+                2-6
+              </option>
+              <option value="6-18" name="age" id="age-3">
+                6-18
+              </option>
+              <option value="18-40" name="age" id="age-4">
+                18-40
+              </option>
+              <option value="40-60" name="age" id="age-5">
+                40-60
+              </option>
+              <option value="Over 60" name="age" id="age-6">
+                Over 60
+              </option>
+            </select>
+          </label>
+          <label>
+            Choose price range:
+            <select value={criteria.price} name="price" onChange={handleChange}>
+              <option value="10" name="price" id="price-1">
+                Under $10
+              </option>
+              <option value="20" name="price" id="price-2">
+                Under $20
+              </option>
+              <option value="50" name="price" id="price-3">
+                Under $50
+              </option>
+              <option value="100" name="price" id="price-4">
+                Under $100
+              </option>
+              <option value="150" name="price" id="price-5">
+                Under $150
+              </option>
+              <option value="200" name="price" id="price-6">
+                Under $200
+              </option>
+              <option value="250" name="price" id="price-7">
+                Over $200
+              </option>
+            </select>
+          </label>
+          <label>
+            Choose hobbies:
+            <select value={criteria.hobby} name="hobby" onChange={handleChange}>
+              <option value="art" name="hobby" id="hobby-art">
+                Art
+              </option>
+              <option value="craft" name="hobby" id="hobby-craft">
+                Craft
+              </option>
+              <option value="creative" name="hobby" id="hobby-creative">
+                Creative
+              </option>
+              <option value="fashion" name="hobby" id="hobby-fashion">
+                Fashion
+              </option>
+              <option value="garderning" name="hobby" id="hobby-garderning">
+                Garderning
+              </option>
+              <option value="household" name="hobby" id="hobby-household">
+                Household
+              </option>
+              <option value="intellectual" name="hobby" id="hobby-intellectual">
+                Intellectual
+              </option>
+              <option value="music" name="hobby" id="hobby-music">
+                Music
+              </option>
+              <option value="natual" name="hobby" id="hobby-natual">
+                Natual Lover
+              </option>
+              <option value="outdoor" name="hobby" id="hobby-outdoor">
+                Outdoor
+              </option>
+              <option value="sport" name="hobby" id="hobby-sport">
+                Sport
+              </option>
+              <option value="technology" name="hobby" id="hobby-technology">
+                Technology
+              </option>
+            </select>
+          </label>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<Search />, document.getElementById("search_result"));
